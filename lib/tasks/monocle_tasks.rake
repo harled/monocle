c31a3a557b069e8098714dd4ab4bce08563abd0a
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2022-10-04 12:47:48 -0700

    engine tests and lib items

---
+++ lib/tasks/monocle_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :monocle do
+#   # Task goes here
+# end
---
+++ test/components/monocle_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MonocleTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert Monocle::VERSION
+  end
+end
