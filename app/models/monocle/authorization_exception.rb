18614b4d981c1cfcdf497ae409b15f5632f97b3d
Jonathan Loos <jonathan.loos@jdcp.forces.gc.ca>
2022-10-16 01:56:03 +0000

    Remove unintended model

--- app/models/monocle/authorization_exception.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Monocle
-  # Represents an occurrence of a user being restricted by authorization
-  class AuthorizationException < ActiveRecord::Base
-  end
-end
