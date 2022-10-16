89869db177ff77e92ce6e3fc2487c2e8ac343dea
Jonathan Loos <jonathan.loos@jdcp.forces.gc.ca>
2022-10-16 02:29:10 +0000

    Update the spec to include the data model

--- app/models/monocle/authorization_exception.rb
+++ app/models/monocle/authorization_exception.rb
@@ -3,5 +3,11 @@ module Monocle
     self.table_name = "authorization_exceptions"
 
     belongs_to :user, polymorphic: true, optional: true
+
+    serialize :backtrace
+
+    after_initialize do |authorization_exception|
+      authorization_exception.backtrace = [] if authorization_exception.backtrace.nil?
+    end
   end
 end
---
+++ spec/sample/db/migrate/20221016021202_create_monocle_authorization_exceptions.rb
@@ -0,0 +1,16 @@
+class CreateMonocleAuthorizationExceptions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notable_jobs do |t|
+      t.string :title
+      t.string :request
+      t.string :controller_name
+      t.string :action_name
+      t.string :exception_class
+      t.text :backtrace, array: true
+
+      t.references :user, polymorphic: true
+
+      t.timestamps
+    end
+  end
+end
