5bbd27278b0fc58f96082eb4236b682fa12c04f5
Jonathan Loos <jonathan.loos@jdcp.forces.gc.ca>
2022-11-08 03:42:25 +0000

    Add exceptions by user component

--- app/components/monocle/exceptions_by_controller_component.rb
+++ app/components/monocle/exceptions_by_controller_component.rb
@@ -5,6 +5,7 @@ module Monocle
   # - The number of exceptions divided by controller
   class ExceptionsByControllerComponent < ApplicationComponent
     def initialize(exceptions:, time_ago: 24.hours)
+      @time_ago = time_ago
       @exceptions_by_controller = []
       exceptions.where(created_at: time_ago.ago..).group(:controller_name).count.each do |controller_name, count|
         @exceptions_by_controller << {name: controller_name, data: count}
--- lib/monocle.rb
+++ lib/monocle.rb
@@ -9,6 +9,7 @@ require_relative "../app/components/monocle/exceptions_since_time_ago_component"
 require_relative "../app/components/monocle/users_affected_since_time_ago_component"
 require_relative "../app/components/monocle/exceptions_by_controller_component"
 require_relative "../app/components/monocle/exceptions_by_request_component"
+require_relative "../app/components/monocle/exceptions_by_user_component"
 
 # models
 require_relative "../app/models/monocle/authorization_exception"
